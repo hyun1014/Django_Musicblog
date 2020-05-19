86900fc866c476ab28752008aac49cc3d80be749
hyun1014 <ksp70712@gmail.com>
2020-05-19 19:38:12 +0900

    bootstrap used

--- musicb/admin.py
+++ musicb/admin.py
@@ -2,9 +2,23 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
+class MemberInline(admin.StackedInline):
+    model = Member
+    extra = 4
+
+class AlbumInline(admin.StackedInline):
+    model = Album
+    extra = 2
+
+class SongInline(admin.StackedInline):
+    model = Song
+    extra = 3
+
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
+    inlines = [MemberInline, AlbumInline, SongInline]
+    
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
